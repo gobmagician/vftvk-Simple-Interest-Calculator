b4346bc6e442960da83e503fd0970d9f4794fe34
Trevor Carpenter <carpentertrev@gmail.com>
2021-06-23 21:15:15 +0000

    sdfasdfasdf

--- script.js
+++ script.js
@@ -3,22 +3,20 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
-    function validate()
-    {
-        if(event.key == "-"){
-            alert("Enter a positive number")
-            return;
-        }
+        function validate()
+        {
+            if(principal<=0){
+                alert("Enter a positive number")
+                return;
+                }
     
-    }
+        }
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
+    var result = document.getElementById("result");
     var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);    
-    document.getElementById("principal").innerHTML = "If you deposit " + principal;
-    document.getElementById("rate").innerHTML = "at an interest rate of " + rate + ".";
-    document.getElementById("interest").innerHTML = "You will receive and amount of " + interest + ",";
-    document.getElementById("year").innerHTML = "in the year " + year + ".";
+    var year = new Date().getFullYear()+parseInt(years);  
+    result.innerHTML="If you deposit " + principal <br> " at an interest rate of " + rate + "." <br> "You will receive and amount of " + interest + "," <br> "in the year " + year + "."
 }
 // Updates output values if the rate slider is adjusted
 function updateRate()
