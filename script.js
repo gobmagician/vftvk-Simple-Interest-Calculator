a3619be4b921240913719db346fd4ccd263b8feb
Trevor Carpenter <carpentertrev@gmail.com>
2021-06-23 19:39:49 +0000

    edited java script

--- script.js
+++ script.js
@@ -2,13 +2,12 @@
 
 function compute()
 {
-    p = document.getElementById("principal").value;
     var principal = document.getElementById("principal").value;
     function validate()
-{
-    if(event.key == "-"){
-        alert("Enter a positive number")
-        return;
+    {
+        if(event.key == "-"){
+            alert("Enter a positive number")
+            return;
     }
     
 }
