9ab5b6f00fd4571c72ece6e4a84f29abdffa9fe6
gobmagician <86016306+gobmagician@users.noreply.github.com>
2021-06-24 13:04:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,18 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
-        function validate()
-        {
-            if(principal<0){
-                alert("Enter a positive number")
-                return;
-                }
-    
-        }
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var result = document.getElementById("result");
     var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);  
-    document.getElementById("result").innerHTML="If you deposit " +  principal  + ",\<br/>at an interest rate of "+rate+".%\<br\>You will recieve an amount of "+interest+",\<br/>in the year "+year+".\<br/>"
+    var year = new Date().getFullYear()+parseInt(years);
+            
+    if(principal<=0){
+          alert("Enter a positive number")
+          return;
+    }
+    else {      
+          document.getElementById("result").innerHTML="If you deposit " +  principal  + ",\<br/>at an interest rate of "+rate+".%\<br\>You will recieve an amount of "+interest+",\<br/>in the year "+year+".\<br/>"}
 }
 // Updates output values if the rate slider is adjusted
 function updateRate()
