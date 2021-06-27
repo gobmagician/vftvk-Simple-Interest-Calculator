f92672926ac87f9e658423dabad2d67af06cfe03
gobmagician <86016306+gobmagician@users.noreply.github.com>
2021-06-26 22:33:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function compute()
 {
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
+    var principal = document.getElementById("principal").style.backgroundColor="yellow".value;
+    var rate = document.getElementById("rate").style.backgroundColor="yellow".value;
+    var years = document.getElementById("years").style.backgroundColor="yellow".value;
     var result = document.getElementById("result");
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
@@ -16,7 +16,7 @@ function compute()
 // Updates output values if the rate slider is adjusted
 function updateRate()
 {
-    var rateval = document.getElementById("rate").value;
+    var rateval = document.getElementById("rate").style.backgroundColor="yellow".value;
     document.getElementById("rate_val").innerText=rateval;
 }
         
